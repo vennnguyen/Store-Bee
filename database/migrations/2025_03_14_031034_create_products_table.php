51df6f842a6de18c5f328730862490ef62fdb2ba
atuan41005 <tuan100205@gmail.com>
2025-03-14 11:56:23 +0700

    create database

---
+++ database/migrations/2025_03_14_031034_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("material");
+            $table->string("price_normal");
+            $table->string("price_sale");
+            $table->longText("description");
+            $table->longText("content");
+            $table->string("image");
+            $table->string("images");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
--- resources/views/admin/components/head.blade.php
+++ resources/views/admin/components/head.blade.php
@@ -1,8 +1,12 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- ajax -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <!-- css -->
 <link rel="stylesheet" href="{{ asset('backend/asset/css/reset.css') }}">
 <link rel="stylesheet" href="{{ asset('backend/asset/css/admin.css') }}">
 <!-- font awesome -->
 <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet" />
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <title>Admin store</title>
